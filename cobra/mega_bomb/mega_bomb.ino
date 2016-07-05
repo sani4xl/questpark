e377e033fd6dbe1260ac9be783f457bab0aafa8f
alex <alex@alexs-MacBook-Pro.local>
2016-07-05 18:09:39 +0300

    no message

--- cobra/mega_bomb/mega_bomb.ino
+++ cobra/mega_bomb/mega_bomb.ino
@@ -107,6 +107,7 @@ boolean canStart = true;
 boolean isWin = false;
 boolean isLost = false;
 boolean exploded = false;
+boolean winDone = false;
 int countDef = 0;
 int wrongWireExtraTime = 15;// 20 ; // in minutes
 
@@ -229,6 +230,8 @@ void startGame(){
   countDef = 0;
   */
   
+  Serial.println(canStart);
+  
   if(!canStart){
     return;
   }
@@ -262,11 +265,13 @@ void startGame(){
   
   isWin = false;
   exploded = false;
+  winDone = false;
   //count();
   
   //delay(500);
   
-  for(int i = 0 ; i < 1; i++){
+  int beepsBeforeStart = 5;
+  for(int i = 0 ; i < beepsBeforeStart; i++){
     beep(500);
   }
   lastStartTime = millis();
@@ -296,9 +301,13 @@ void initCable(){
 
 
 void checkWires(){
+  
+  
   if(!onUpActivated){
     return;
   }
+  
+  
   
   for(int i  = 0; i < 4; i++){
     int isWireOn = digitalRead(defuseOptions[0][i]);
@@ -332,8 +341,19 @@ void checkWires(){
   
   if(wireCutIndex >= 4){
     isBombDefused = true; 
-    //Serial.println (subDefuseTime);
+    shutDownAnturage();
+
   }
+}
+
+void doWin(){
+  if(winDone){
+    return;
+  }
+  
+  winDone = true;
+  shutDownAnturage();
+  playStarWars();
 }
 
 void explode(){
@@ -362,6 +382,8 @@ void explode(){
   movingUp = false;
   delay(explosionTime);
   digitalWrite(explodePin, HIGH); 
+  
+  playDartWader();
 }
 
 void shutDownAnturage(){
@@ -396,6 +418,8 @@ void loop()
      return;
    }
    
+   //Serial.println(onUpActivated);
+   
   if(isLost){
     explode();
     Serial.println("LOST");
@@ -423,7 +447,7 @@ void lifting(){
   }
   else{
     //Serial.println("down reached");  
-    canStart = true;
+    //canStart = true;
   }
  
  stopperUpState = digitalRead(stopperUp);
@@ -443,13 +467,13 @@ void lifting(){
     if(!onUpPreActivated){
       onUpPreActivated = true;
     }
-    //Serial.println("up reached");  
+    Serial.println("up reached");  
     digitalWrite(lightPin, LOW);
     
   }
   
   if(digitalRead(buttonStrartPin)){
-    //Serial.println("start button pressed");  
+    Serial.println("start button pressed");  
     startGame();
     
   }
@@ -531,9 +555,11 @@ void count() {
        preUpActivatedSeconds = secondsPassed;
        //onUpPreActivated = true;
        
+       
     }
     
-    if(!onUpActivated && preUpActivatedSeconds >= 0 && (secondsPassed - preUpActivatedSeconds) > 10 ){
+    if(!onUpActivated && preUpActivatedSeconds > 0 && (secondsPassed - preUpActivatedSeconds) > 10 ){
+      
       onUpActivated = true;
     }
     
@@ -596,6 +622,7 @@ void count() {
     
    if(isBombDefused){
      isWin = true; 
+     doWin();
     }
     else if(secondsLeft <= 0 ){
       isLost = true;
@@ -637,7 +664,7 @@ void timerRunDown(){
   //
 void displayNumber(String line){
    
-   //Serial.println(line); 
+
    counter = line.toInt();
    sprintf(tempString, "%4d", counter);
    String timeStringToDispay = (String)tempString;
@@ -703,7 +730,7 @@ void s7sSendStringSPI(String toSend)
 {
   digitalWrite(ssPin, LOW);
  
-  //Serial.println(toSend);
+
   for (int i=0; i<4; i++)
   {
     SPI.transfer((byte)toSend[i]);
@@ -820,4 +847,118 @@ void stopCdMotor(){
 void stopLiqMotor(){
   digitalWrite(liqMotorA, LOW);
   digitalWrite(liqMotorB, LOW);
+}
+
+#define  c3    7634
+#define  d3    6803
+#define  e3    6061
+#define  f3    5714
+#define  g3    5102
+#define  a3    4545
+#define  b3    4049
+#define  c4    3816    // 261 Hz 
+#define  d4    3401    // 294 Hz 
+#define  e4    3030    // 329 Hz 
+#define  f4    2865    // 349 Hz 
+#define  g4    2551    // 392 Hz 
+#define  a4    2272    // 440 Hz 
+#define  a4s   2146
+#define  b4    2028    // 493 Hz 
+#define  c5    1912    // 523 Hz
+#define  d5    1706
+#define  d5s   1608
+#define  e5    1517    // 659 Hz
+#define  f5    1433    // 698 Hz
+#define  g5    1276
+#define  a5    1136
+#define  a5s   1073
+#define  b5    1012
+#define  c6    955
+ 
+#define  R     0 
+
+int melody1[] = {  a4, R,  a4, R,  a4, R,  f4, R, c5, R,  a4, R,  f4, R, c5, R, a4, R,  e5, R,  e5, R,  e5, R,  f5, R, c5, R,  g5, R,  f5, R,  c5, R, a4, R};
+int beats1[]  = {  50, 20, 50, 20, 50, 20, 40, 5, 20, 5,  60, 10, 40, 5, 20, 5, 60, 80, 50, 20, 50, 20, 50, 20, 40, 5, 20, 5,  60, 10, 40, 5,  20, 5, 60, 40};
+ 
+// Melody 2: Star Wars Theme
+int melody2[] = {  f4,  f4, f4,  a4s,   f5,  d5s,  d5,  c5, a5s, f5, d5s,  d5,  c5, a5s, f5, d5s, d5, d5s,   c5};
+int beats2[]  = {  21,  21, 21,  128,  128,   21,  21,  21, 128, 64,  21,  21,  21, 128, 64,  21, 21,  21, 128 }; 
+ 
+int MAX_COUNT = sizeof(melody2) / 2; // Melody length, for looping.
+ 
+long tempo = 10000; // Set overall tempo
+ 
+int pause = 1000; // Set length of pause between notes
+ 
+int rest_count = 50; // Loop variable to increase Rest length (BLETCHEROUS HACK; See NOTES)
+ 
+// Initialize core variables
+int toneM = 0;
+int beat = 0;
+long melodDuration  = 0;
+
+ 
+// PLAY TONE  //
+// Pulse the speaker to play a tone for a particular duration
+void playTone() {
+  long elapsed_time = 0;
+  if (toneM > 0) { // if this isn't a Rest beat, while the tone has 
+    //  played less long than 'duration', pulse speaker HIGH and LOW
+    while (elapsed_time < melodDuration) {
+ 
+      //digitalWrite(speakerOut,HIGH);
+      analogWrite (speakerPin, 0);
+      delayMicroseconds(toneM / 2);
+ 
+      // DOWN
+      //digitalWrite(speakerOut, LOW);
+      analogWrite (speakerPin, 255);
+      delayMicroseconds(toneM / 2);
+ 
+      // Keep track of how long we pulsed
+      elapsed_time += (toneM);
+    } 
+  }
+  else { // Rest beat; loop times delay
+    for (int j = 0; j < rest_count; j++) { // See NOTE on rest_count
+      delayMicroseconds(melodDuration);  
+    }                                
+  }                                 
+}
+ 
+void playDartWader(){
+   int MAX_COUNT = sizeof(melody1) / 2;
+   for(int j = 0 ; j < 2; j++){ 
+     for (int i=0; i<MAX_COUNT; i++) {
+       toneM = melody1[i];
+       beat = beats1[i];
+ 
+       melodDuration = beat * tempo; // Set up timing
+ 
+      playTone(); // A pause between notes
+      delayMicroseconds(pause);
+    }
+   }
+    
+    analogWrite (speakerPin, 255);
+}
+
+void playStarWars(){
+  
+   int MAX_COUNT = sizeof(melody2) / 2;
+   for(int j = 0 ; j < 2; j++){ 
+     for (int i=0; i<MAX_COUNT; i++) {
+      toneM = melody2[i];
+      beat = beats2[i];
+ 
+      melodDuration = beat * tempo; // Set up timing
+ 
+      playTone(); // A pause between notes
+      delayMicroseconds(pause);
+     }
+   }  
+   
+   analogWrite (speakerPin, 255);
+    
+
 }
