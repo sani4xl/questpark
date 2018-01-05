24d903d7c0c96ced5020cc46863225cae3267a45
manjulife <vicmanjul@gmail.com>
2018-01-05 17:48:52 +0300

    main updated

--- karantin/main/main.ino
+++ karantin/main/main.ino
@@ -1,16 +1,29 @@
+#include <Bounce2.h>
 
-
-const int labRelePin = 22;
+const int labRelePin = 3;
+const int buttonPin = 4;
 int prevLabReleMillis;
 int nextLabReleMillisSwitch = 0;
 boolean labLightOn = true;
+
+boolean labLightSwitchingActive = true;
+
+int switchMode = 0;
+
+Bounce buttonBounce = Bounce();
 
 void setup()
 {
   Serial.begin(9600);
 
+  buttonBounce.attach(buttonPin);
+  buttonBounce.interval(10); 
+
+  labLightSwitchingActive = false;
+
   
   pinMode (labRelePin, OUTPUT);
+  switchLightOn();
  
   
   
@@ -20,11 +33,35 @@ void setup()
 
 void loop()
 {
+  buttonBounce.update();
 
-  prevLabReleMillis = millis();
-  if(prevLabReleMillis > nextLabReleMillisSwitch){
-    nextLabReleMillisSwitch = prevLabReleMillis + random(100, 2000);
-    doLabSwitch();
+  if(buttonBounce.rose()){
+
+    switchMode++;
+    if(switchMode>=3){
+      switchMode = 0;
+    }
+
+    if(switchMode == 0){
+      labLightSwitchingActive = false;
+      switchLightOn();
+    }
+    else if(switchMode == 2){
+      labLightSwitchingActive = false;
+      switchLightOff();
+    }
+    else{
+      labLightSwitchingActive = true;
+    }
+    
+  }
+
+  if(labLightSwitchingActive){
+    prevLabReleMillis = millis();
+    if(prevLabReleMillis > nextLabReleMillisSwitch){
+      nextLabReleMillisSwitch = prevLabReleMillis + random(100, 2000);
+      doLabSwitch();
+    }
   }
   
  
@@ -40,13 +77,13 @@ void doLabSwitch(){
 }
 
 void switchLightOn(){
-   digitalWrite(labRelePin, LOW);
+   digitalWrite(labRelePin, HIGH);
    labLightOn = true;
   
 }
 
 void switchLightOff(){
-   digitalWrite(labRelePin, HIGH);
+   digitalWrite(labRelePin, LOW);
    labLightOn = false;
   
 }
